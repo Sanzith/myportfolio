1fc47dbfc56f91ebea8b8f63987fe36681a82e7a
Sanzith Alam <121573768+Sanzith@users.noreply.github.com>
2025-02-26 23:53:28 +0600

    Update FooterBottom.js

--- src/components/footer/FooterBottom.js
+++ src/components/footer/FooterBottom.js
@@ -3,7 +3,7 @@ import React from 'react'
 const FooterBottom = () => {
   return (
     <div className="w-full py-10">
-      <p className="text-center text-gray-500 text-base">
+      <p className="text-center text-gray-501 text-base">
         © 2025. All rights reserved by Sanzith
       </p>
     </div>
